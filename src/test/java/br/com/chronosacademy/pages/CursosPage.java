2953556bc6f44f355490d8146863205b0857b9ea
Danilo Gripa <danilogripa@gmail.com>
2021-09-25 17:23:37 -0300

    Removing Imports not needed

--- src/test/java/br/com/chronosacademy/pages/CursosPage.java
+++ src/test/java/br/com/chronosacademy/pages/CursosPage.java
@@ -1,9 +1,7 @@
 package br.com.chronosacademy.pages;
 
 import br.com.chronosacademy.maps.CursoMap;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
 public class CursosPage {
