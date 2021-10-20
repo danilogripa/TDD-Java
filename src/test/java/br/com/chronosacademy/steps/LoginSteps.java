a2c046c73bda5f0e7d3cef74df0858ec682ba5b4
Danilo Gripa <danilogripa@gmail.com>
2021-10-20 06:34:35 -0300

    Steps and waits

--- src/test/java/br/com/chronosacademy/steps/LoginSteps.java
+++ src/test/java/br/com/chronosacademy/steps/LoginSteps.java
@@ -1,20 +1,40 @@
 package br.com.chronosacademy.steps;
 
+import br.com.chronosacademy.core.Driver;
+import br.com.chronosacademy.pages.LoginPage;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
 import io.cucumber.java.pt.Entao;
+import org.junit.Assert;
+
+import java.util.Map;
 
 public class LoginSteps {
+
+    LoginPage loginPage;
+
+    @Before
+    public void iniciaNavegador(){
+        new Driver("chrome");
+    }
+
+    @After
+    public void fechaNavegador(){
+        Driver.getDriver().quit();
+    }
+
     @Dado("que a modal esteja sendo exibida")
     public void queAModalEstejaSendoExibida() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        Driver.getDriver().get("https://www.advantageonlineshopping.com/");
+        loginPage = new LoginPage();
+        loginPage.clickBtnLogin();
     }
     @Quando("for realizado um click fora da modal")
     public void forRealizadoUmClickForaDaModal() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        loginPage.clickDivFechaModal();
     }
     @Entao("a janela modal deve ser fechada")
     public void aJanelaModalDeveSerFechada() {
@@ -24,36 +44,51 @@ public class LoginSteps {
 
     @Quando("for realizado um click no icone Fechar")
     public void forRealizadoUmClickNoIconeFechar() {
-
+        loginPage.clickBtnFechar();
     }
 
     @Quando("for realizado um click no Create New Account")
     public void forRealizadoUmClickNoCreateNewAccount() {
-
+        loginPage.clickLinkCreateAccount();
     }
 
     @Então("a pagina Create New Account deve ser exibida")
     public void aPaginaCreateNewAccountDeveSerExibida() {
-
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @Quando("os campos de login sejam preenchidos da seguinte forma")
-    public void osCamposDeLoginSejamPreenchidosDaSeguinteForma() {
+    public void osCamposDeLoginSejamPreenchidosDaSeguinteForma(Map<String, String> map) {
+        String username = map.get("login");
+        String password = map.get("password");
+        boolean remember = Boolean.parseBoolean(map.get("remember"));
+
+        loginPage.setInpUserName(username);
+        loginPage.setInpPassword(password);
+        if (remember) loginPage.clickInpRemember();
     }
 
     @Quando("for realizado o click no botao sign in")
     public void forRealizadoOClickNoBotaoSignIn() {
+        loginPage.clickBtnSignIn();
     }
 
     @Entao("deve ser possivel logar no sistema")
     public void deveSerPossivelLogarNoSistema() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @Entao("o sistema deve exibir uma msg de erro")
     public void oSistemaDeveExibirUmaMsgDeErro() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @Entao("o botao sign in deve permanecer desabilitado")
     public void oBotaoSignInDevePermanecerDesabilitado() {
+        boolean enabled = loginPage.isBtnSignIn();
+        Assert.assertFalse(enabled);
     }
 }
